5ab3d609b75b1ed3d363903afbd4276a02dcdb49
Herman Bastesen <herman.bastesen@outlook.com>
2021-01-08 13:45:00 +0100

    Working waker thread

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Threading;
 
 namespace Keepwake
 {
@@ -10,21 +11,35 @@ namespace Keepwake
         private ContextMenu contextMenu;
         private MenuItem menuItemExit;
         private System.ComponentModel.IContainer components;
-        enum Mode
+
+        //Create helper thread for keeping windows awake
+        private static ManualResetEventSlim mre = new ManualResetEventSlim(false);
+        private Thread wakerThread = new Thread(new ThreadStart(Waker));
+        
+        //Application can be in two states: Prevent Windows from sleeping or allow it
+        enum State : int
         {
-            KeepWake,
-            AllowSleep
+            KeepWake = 0,
+            AllowSleep = 1
         }
-        private Mode mode = Mode.AllowSleep;
+        String[] stateCaptions =
+        {
+            "Prevent sleep",
+            "Allow sleep"
+        };
+        private State state = State.AllowSleep;
 
         static void Main(string[] args)
         {
+            //Initialize program as running in the background with the task bar icon as only interface
             Program program = new Program();
             Application.Run();
             Console.ReadLine();
         }
         Program()
         {
+            wakerThread.Start();
+            mre.Reset();
             CreateTaskbarIcon();
         }
 
@@ -38,6 +53,7 @@ namespace Keepwake
             this.menuItemExit.Text = "Exit";
             this.menuItemExit.Click += new EventHandler(this.Exit);
 
+            //Add all menu items to context menu
             this.contextMenu.MenuItems.AddRange(
                 new MenuItem[] { this.menuItemExit });
 
@@ -47,34 +63,53 @@ namespace Keepwake
 
             this.notifyIcon.ContextMenu = this.contextMenu;
 
-            notifyIcon.Text = "KeepWake";
+            notifyIcon.Text = "KeepWake: Allow Sleep";
+            notifyIcon.Text = String.Format("KeepWake: {0}", stateCaptions[(int) this.state]);
             notifyIcon.Visible = true;
 
             notifyIcon.Click += new EventHandler(this.ToggleMode);
-
         }
 
         private void ToggleMode(object Sender, EventArgs e)
         {
             //Called by clicking the task bar icon
-            if (this.mode == Mode.AllowSleep)
+            if (this.state == State.AllowSleep)
             {
-                this.mode = Mode.KeepWake;
+                this.state = State.KeepWake;
                 this.notifyIcon.Icon = new Icon("filled.ico");
+                Console.WriteLine("Staring waker");
+                mre.Set();
             }
             else
             {
-                this.mode = Mode.AllowSleep;
+                this.state = State.AllowSleep;
                 this.notifyIcon.Icon = new Icon("empty.ico");
+                Console.WriteLine("Suspending waker");
+                mre.Reset();
             }
 
-
+            notifyIcon.Text = String.Format("KeepWake: {0}", stateCaptions[(int)this.state]);
         }
 
         private void Exit(object Sender, EventArgs e)
         {
             //Called by clicking the exit button in the menu
+            wakerThread.Abort();
             Application.Exit();
+        }
+
+        private static void Waker()
+        {
+            //The waker thread will run in the background if application is in state KeepWake.
+            //It will periodically simulate a keypress to prevent sleep
+            //When in the AllowSleep state it will wait until it is once again told to keep Windows awake
+            while (true)
+            {
+                mre.Wait();
+                System.Threading.Thread.Sleep(60 * 1000);
+                Console.WriteLine("Sending wake signal to Windows");
+                SendKeys.SendWait("{F15}");
+            }
         }
     }
 }
